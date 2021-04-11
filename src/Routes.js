76b09cebcc732831c2f168acc8736b8f3f26e4f3
unknown <lindos-mahadi>
2021-04-11 18:18:01 +0600

    change messages before running eject

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
-import {Route, Switch} from "react-router-dom";
-import Home from './components/menu/Home';
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-export default class Routes extends Component {
-    render() {
-        return (
-            <Switch>
-                <Routes path="/" exact component={Home} />
-            </Switch>
+import Home from "./components/menu/Home";
+import Layout from './hoc/Layout/layout';
+
+// import NewsArticle from './components/Articles/News/Post/index';
+// import VideoArticle from './components/Articles/Videos/Video/index'
+// import NewsMain from './components/Articles/News/Main/index';
+// import VideosMain from './components/Articles/Videos/Main/index';
+
+class Routes extends Component {
+    render(){
+        return(
+            <Layout>
+                <Switch>
+                    <Route path="/" exact component={Home}/>
+                    {/* <Route path="/news" exact component={NewsMain}/>
+                    <Route path="/articles/:id" exact component={NewsArticle}/>
+                    <Route path="/videos/:id" exact component={VideoArticle}/>
+                    <Route path="/videos" exact component={VideosMain}/> */}
+                </Switch>
+            </Layout>
+           
         )
     }
 }
+
+export default Routes;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from "react-router-dom";
+// import App from './App';
 import './index.css';
 import Routes from './Routes';
 
-const App =()=>{
-      return(
-        <BrowserRouter>
-          <Routes />
-        </BrowserRouter>
-      )
+const App = () => {
+  return(
+      <BrowserRouter>
+          <Routes/>
+      </BrowserRouter>
+  )
 }
 
 ReactDOM.render(
