c51ef609c3e1c84be11f3b3cc4f310082f240a2c
unknown <lindos-mahadi>
2021-04-24 17:27:00 +0600

    Slider and Articles added

--- src/components/widgets/newsSlider/Slider.js
+++ src/components/widgets/newsSlider/Slider.js
@@ -1,28 +1,31 @@
 import axios from 'axios'
 import React, { Component } from 'react'
-import SliderTemplate from './SliderTemplete';
+import SliderTemplates from './SliderTemplete'
 
-export default class Slider extends Component {
+class Slider extends Component {
 
-    state={
-        news: []
+    state = {
+        news:[]
     }
 
-    componentDidMount(){
-        axios.get(`http://localhost:3005/articles? start=0&_end=5`)
-        .then(response=>{
+    componentWillMount(){
+        axios.get(`http://localhost:3005/articles?_start=0&_end=5`)
+        .then( response => {
             this.setState({
-                news:response.data
+                news:response.data 
             })
         })
     }
 
     render() {
-        // console.log(this.state.news)
+        console.log(this.state.news)
         return (
             <div>
-                <SliderTemplate data={this.state.news} />
+                {/* <SliderTemplate data={this.state.news} type={this.props.type} settings={this.props.settings} /> */}
+            {/* slider */}
+            <SliderTemplates data={this.state.news} type="featured" />
             </div>
         )
     }
 }
+export default Slider;
